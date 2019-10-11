c41fc90a6ab293b12c1b906e692a13009516a468
Gerald Ruiz <gruiz@eogresources.com>
2019-10-11 16:09:25 -0500

    til interfaces

--- railAPI/improvedRect.go
+++ railAPI/improvedRect.go
@@ -21,7 +21,7 @@ func distance(x1, y1, x2, y2 float64) float64 {
 	return math.Sqrt(a*a + b*b)
 }
 
-func (r *Rectangle) rectArea() float64 {
+func (r *Rectangle) area() float64 {
 	l := distance(r.x1, r.y1, r.x1, r.y2)
 	w := distance(r.x1, r.y1, r.x2, r.y1)
 	return l * w
@@ -37,5 +37,5 @@ func main() {
 	r := Rectangle{0, 0, 10, 10}
 
 	fmt.Println(c.area())
-	fmt.Println(r.rectArea())
+	fmt.Println(r.area())
 }
